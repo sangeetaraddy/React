c2fdd7f2a33ffb8cf739c45121889c36fb526ee5
sangeetaraddy <shr.sangeetaraddy@gmail.com>
2024-01-26 17:27:56 +0530

    react hook for navigation

--- Applications/courselistapp/src/Components/EnquireForm.js
+++ Applications/courselistapp/src/Components/EnquireForm.js
@@ -1,32 +1,168 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import './EnquireForm.css';
+import { useParams ,useNavigate} from "react-router-dom";
 
-const url = "http://localhost:6700/course";
 
-class EnquireForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { details: '' }
-    }
-    componentDidMount() {
-        let id = 1;
-        fetch(`${url}/${id}`, {
-            method: 'GET'
-        })
+// class EnquireForm extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { details: '' }
+//     }
+//     componentDidMount() {
+// let id = 1;
+// fetch(`${url}/${id}`, {
+//     method: 'GET'
+// })
+//     .then(response => response.json())
+//     .then((data) => {
+//         this.setState({
+//             details: data
+//         })
+//     })
+//     }
+
+//     render() {
+//         return (
+//             <div style={{ paddingTop: '78px' }}>
+//                 <div className="container">
+//                     <h4 className="text-banner-title">
+//                     {details.title}
+//                     </h4>
+//                     <hr></hr>
+//                     <div className="row">
+//                         <div className="col-md-7 col-xs-12">
+//                             <h4 className="text-banner-subtitle">
+//                                 Enter your details to enquire about this course</h4>
+//                             <div className="row">
+//                                 <div className="col-md-6 col-xs-12">
+//                                     <div className="form-group">
+//                                         <label>Name <sub className="text-danger text-md-start">*</sub> </label>
+//                                         <input id="billing-name" type="text" className="form-control form-control-md rounded-3" name="billing_name" />
+//                                         <small></small>
+//                                     </div>
+//                                 </div>
+//                                 <div className="col-md-6 col-xs-12">
+//                                     <div className="form-group">
+//                                         <label>Contact No. <sub className="text-danger text-md-start">*</sub></label>
+//                                         <input id="billing-tel" type="text" className="form-control form-control-md rounded-3" name="billing_tel" />
+//                                         <small></small>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div className="form-group">
+//                                 <label>Email address <sub className="text-danger text-md-start">*</sub></label>
+//                                 <input id="billing-email" type="email" className="form-control form-control-md rounded-3" name="billing_email" />
+//                                 <small></small>
+//                             </div>
+//                             <div className="row">
+//                                 <div className="col-md-6 col-xs-12">
+//                                     <div className="form-group">
+//                                         <label>Country <sub className="text-danger text-md-start">*</sub></label>
+//                                         <select id="country" name="billing_country" className="form-control rounded-3" aria-label="Default select example">
+//                                             <option value="India" defaultValue="">India</option>
+//                                             <option value="United Kingdom">United Kingdom</option>
+//                                             <option value="USA">USA</option>
+//                                             <option value="Other">Other</option>
+//                                         </select>
+//                                         <small></small>
+//                                     </div>
+//                                 </div>
+//                                 <div className="col-md-6 col-xs-12">
+//                                     <div className="form-group">
+//                                         <label>State Name <sub className="text-danger text-md-start">*</sub></label>
+//                                         <input id="billing-state" type="text" className="form-control form-control-md rounded-3" name="billing_state" />
+//                                         <small></small>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                             <div className="row">
+//                                 <div className="col-md-10"></div>
+//                                 <div className="col-md-2">
+//                                     <button className="btn btn-success">Submit</button>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div className="col-md-5 mb-4">
+//                             <div className="course-learn-list">
+//                                 <div className="row ">
+//                                     <div className="col-md-6 col-xs-12">
+//                                         <h4>
+//                                             Selected Course
+//                                         </h4>
+//                                     </div>
+//                                 </div>
+//                                 <div className="row">
+//                                     <div className="col-md-12 col-xs-12">
+//                                         <h5> {details.title}</h5>
+//                                     </div>
+//                                     <div className="col-md-12 col-xs-12">
+//                                     {details.topic}
+//                                     </div>
+//                                     <div className="col-md-12 col-xs-12">
+//                                         Price :  {details.price}
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+const EnquireForm = (props) => {
+    const courseurl = "http://localhost:6700/course";
+    const enquiriesurl = "http://localhost:6700/enquiries";
+    const navigate = useNavigate();
+    const { id } = useParams();
+    let [details, setDetails] = useState({});
+    const [state, setState] = useState({
+        name: "",
+        contact: "",
+        email: "",
+        course: "",
+        country: "India",
+        state: ""
+    });
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(state);
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(state)
+        };
+        fetch(`${enquiriesurl}`, requestOptions)
             .then(response => response.json())
-            .then((data) => {
-                this.setState({
-                    details: data
-                })
+            .then(data => {
+                navigate("/EnquiriesList")
             })
-    }
+    };
+    const handleInputChange = (event) => {
+        //event.preventDefault();
+        const { name, value } = event.target;
+        setState((prevProps) => ({
+            ...prevProps,
+            [name]: value
+        }));
 
-    render() {
-        return (
-            <div style={{ paddingTop: '78px' }}>
+    };
+    useEffect(() => {
+        fetch(`${courseurl}/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                setDetails(data);
+                setState({ course: data.title })
+            })
+    }, [])
+
+    return (
+
+        <div style={{ paddingTop: '78px' }}>
+            <form onSubmit={handleSubmit}>
                 <div className="container">
                     <h4 className="text-banner-title">
-                    {this.state.details.title}
+                        {details.title}
                     </h4>
                     <hr></hr>
                     <div className="row">
@@ -37,29 +173,49 @@ class EnquireForm extends Component {
                                 <div className="col-md-6 col-xs-12">
                                     <div className="form-group">
                                         <label>Name <sub className="text-danger text-md-start">*</sub> </label>
-                                        <input id="billing-name" type="text" className="form-control form-control-md rounded-3" name="billing_name" />
+                                        <input
+                                            className="form-control form-control-md rounded-3"
+                                            type="text"
+                                            name="name"
+                                            value={state.name}
+                                            onChange={handleInputChange} />
                                         <small></small>
                                     </div>
                                 </div>
                                 <div className="col-md-6 col-xs-12">
                                     <div className="form-group">
                                         <label>Contact No. <sub className="text-danger text-md-start">*</sub></label>
-                                        <input id="billing-tel" type="text" className="form-control form-control-md rounded-3" name="billing_tel" />
+                                        <input
+                                            type="text"
+                                            name="contact"
+                                            value={state.contact}
+                                            onChange={handleInputChange}
+                                            className="form-control form-control-md rounded-3" />
                                         <small></small>
                                     </div>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label>Email address <sub className="text-danger text-md-start">*</sub></label>
-                                <input id="billing-email" type="email" className="form-control form-control-md rounded-3" name="billing_email" />
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={state.email}
+                                    onChange={handleInputChange}
+                                    className="form-control form-control-md rounded-3" />
                                 <small></small>
                             </div>
                             <div className="row">
                                 <div className="col-md-6 col-xs-12">
                                     <div className="form-group">
                                         <label>Country <sub className="text-danger text-md-start">*</sub></label>
-                                        <select id="country" name="billing_country" className="form-control rounded-3" aria-label="Default select example">
-                                            <option value="India" defaultValue="">India</option>
+                                        <select
+                                            type="text"
+                                            name="country"
+                                            value={state.country}
+                                            onChange={handleInputChange}
+                                            className="form-control rounded-3" aria-label="Default select example">
+                                            <option value="India">India</option>
                                             <option value="United Kingdom">United Kingdom</option>
                                             <option value="USA">USA</option>
                                             <option value="Other">Other</option>
@@ -70,7 +226,12 @@ class EnquireForm extends Component {
                                 <div className="col-md-6 col-xs-12">
                                     <div className="form-group">
                                         <label>State Name <sub className="text-danger text-md-start">*</sub></label>
-                                        <input id="billing-state" type="text" className="form-control form-control-md rounded-3" name="billing_state" />
+                                        <input
+                                            type="text"
+                                            name="state"
+                                            value={state.state}
+                                            onChange={handleInputChange}
+                                            className="form-control form-control-md rounded-3" />
                                         <small></small>
                                     </div>
                                 </div>
@@ -78,7 +239,7 @@ class EnquireForm extends Component {
                             <div className="row">
                                 <div className="col-md-10"></div>
                                 <div className="col-md-2">
-                                    <button className="btn btn-success">Submit</button>
+                                    <button type="submit" className="btn btn-success">Submit</button>
                                 </div>
                             </div>
                         </div>
@@ -93,21 +254,28 @@ class EnquireForm extends Component {
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12 col-xs-12">
-                                        <h5> {this.state.details.title}</h5>
+                                        <h5> {details.title}</h5>
                                     </div>
                                     <div className="col-md-12 col-xs-12">
-                                    {this.state.details.topic}
+                                        {details.topic}
+                                        <input
+                                            type="hidden"
+                                            name="course"
+                                            value={state.course}
+                                            onChange={handleInputChange}
+                                            className="form-control form-control-md rounded-3" />
+
                                     </div>
                                     <div className="col-md-12 col-xs-12">
-                                        Price :  {this.state.details.price}
+                                        Price :  {details.price}
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
+            </form>
+        </div>
+    )
 }
 export default EnquireForm;
--- Applications/courselistapp/src/Components/UserEnquiries.js
+++ Applications/courselistapp/src/Components/UserEnquiries.js
@@ -1,57 +1,60 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class UserEnquiries extends Component {
-    render() {
+const UserEnquiries = () => {
+    const enquiriesurl = "http://localhost:6700/enquiries";
+    let [data, setData] = useState([]);
+    const tdList = data.map((i) => {
         return (
-            <div style={{ paddingTop: '78px' }}>
-                <div className="container">
-                    <h4 className="text-banner-title">
-                      Users Enquiries
-                    </h4>
-                   
-                    <div className="row">
-                        <table className="table" style={{backgroundColor:"white"}}>
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">User Name</th>
-                                    <th scope="col">Contact No.</th>
-                                    <th scope="col">Email Id</th>
-                                    <th scope="col">Course</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>Azure</td>
+            <tr>
+              
+                <td>
+                    {i.name}
+                </td>
+                <td>
+                    {i.contact}
+                </td>
+                <td>
+                    {i.email}
+                </td>
+                <td>
+                    {i.course}
+                </td>
+            </tr>
+        );
+    });
+    useEffect(() => {
+        fetch(`${enquiriesurl}`)
+            .then(response => response.json())
+            .then(data => {
+                setData(data);
+            })
+    }, [])
+    return (
+        <div style={{ paddingTop: '78px' }}>
+            <div className="container">
+                <h4 className="text-banner-title">
+                    Users Enquiries
+                </h4>
 
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>Azure</td>
-
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                    <td>Azure</td>
-                                </tr>
-                                
-                            </tbody>
-                        </table>
-                    </div>
+                <div className="row">
+                    <table className="table" style={{ backgroundColor: "white" }}>
+                        <thead>
+                            <tr>
+                              
+                                <th scope="col">User Name</th>
+                                <th scope="col">Contact No.</th>
+                                <th scope="col">Email Id</th>
+                                <th scope="col">Course</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {tdList}
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default UserEnquiries;
